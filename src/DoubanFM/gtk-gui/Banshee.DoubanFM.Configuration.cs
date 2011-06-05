61ae4a0a42f79251b86c03c402ac8cfddc6dabbe
Bill Chen <pro711@gmail.com>
2011-06-06 00:06:58 +0800

    Polish configuration dialog.

--- src/DoubanFM/gtk-gui/Banshee.DoubanFM.Configuration.cs
+++ src/DoubanFM/gtk-gui/Banshee.DoubanFM.Configuration.cs
@@ -4,17 +4,15 @@ namespace Banshee.DoubanFM
 {
 	public partial class Configuration
 	{
-		private global::Gtk.HBox hbox1;
+		private global::Gtk.Table table1;
 
 		private global::Gtk.Label label1;
-
-		private global::Gtk.Entry username;
-
-		private global::Gtk.HBox hbox2;
 
 		private global::Gtk.Label label2;
 
 		private global::Gtk.Entry password;
+
+		private global::Gtk.Entry username;
 
 		private global::Gtk.Button buttonCancel;
 
@@ -33,66 +31,65 @@ namespace Banshee.DoubanFM
 			w1.Name = "ConfigurationDialog_VBox";
 			w1.BorderWidth = ((uint)(2));
 			// Container child ConfigurationDialog_VBox.Gtk.Box+BoxChild
-			this.hbox1 = new global::Gtk.HBox ();
-			this.hbox1.Name = "hbox1";
-			this.hbox1.Spacing = 6;
-			// Container child hbox1.Gtk.Box+BoxChild
+			this.table1 = new global::Gtk.Table (((uint)(2)), ((uint)(2)), false);
+			this.table1.Name = "table1";
+			this.table1.RowSpacing = ((uint)(6));
+			this.table1.ColumnSpacing = ((uint)(6));
+			// Container child table1.Gtk.Table+TableChild
 			this.label1 = new global::Gtk.Label ();
 			this.label1.Name = "label1";
 			this.label1.LabelProp = global::Mono.Unix.Catalog.GetString ("Email");
-			this.hbox1.Add (this.label1);
-			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.hbox1[this.label1]));
-			w2.Position = 0;
-			w2.Expand = false;
-			w2.Fill = false;
-			// Container child hbox1.Gtk.Box+BoxChild
-			this.username = new global::Gtk.Entry ();
-			this.username.CanFocus = true;
-			this.username.Name = "username";
-			this.username.IsEditable = true;
-			this.username.InvisibleChar = '●';
-			this.hbox1.Add (this.username);
-			global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.hbox1[this.username]));
-			w3.Position = 1;
-			w1.Add (this.hbox1);
-			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(w1[this.hbox1]));
-			w4.Position = 0;
-			w4.Expand = false;
-			w4.Fill = false;
-			// Container child ConfigurationDialog_VBox.Gtk.Box+BoxChild
-			this.hbox2 = new global::Gtk.HBox ();
-			this.hbox2.Name = "hbox2";
-			this.hbox2.Spacing = 6;
-			// Container child hbox2.Gtk.Box+BoxChild
+			this.table1.Add (this.label1);
+			global::Gtk.Table.TableChild w2 = ((global::Gtk.Table.TableChild)(this.table1[this.label1]));
+			w2.XOptions = ((global::Gtk.AttachOptions)(4));
+			w2.YOptions = ((global::Gtk.AttachOptions)(4));
+			// Container child table1.Gtk.Table+TableChild
 			this.label2 = new global::Gtk.Label ();
 			this.label2.Name = "label2";
 			this.label2.LabelProp = global::Mono.Unix.Catalog.GetString ("Password");
-			this.hbox2.Add (this.label2);
-			global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.hbox2[this.label2]));
-			w5.Position = 0;
-			w5.Expand = false;
-			w5.Fill = false;
-			// Container child hbox2.Gtk.Box+BoxChild
+			this.table1.Add (this.label2);
+			global::Gtk.Table.TableChild w3 = ((global::Gtk.Table.TableChild)(this.table1[this.label2]));
+			w3.TopAttach = ((uint)(1));
+			w3.BottomAttach = ((uint)(2));
+			w3.XOptions = ((global::Gtk.AttachOptions)(4));
+			w3.YOptions = ((global::Gtk.AttachOptions)(4));
+			// Container child table1.Gtk.Table+TableChild
 			this.password = new global::Gtk.Entry ();
 			this.password.CanFocus = true;
 			this.password.Name = "password";
 			this.password.IsEditable = true;
 			this.password.Visibility = false;
 			this.password.InvisibleChar = '●';
-			this.hbox2.Add (this.password);
-			global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(this.hbox2[this.password]));
-			w6.Position = 1;
-			w1.Add (this.hbox2);
-			global::Gtk.Box.BoxChild w7 = ((global::Gtk.Box.BoxChild)(w1[this.hbox2]));
-			w7.Position = 1;
-			w7.Expand = false;
-			w7.Fill = false;
+			this.table1.Add (this.password);
+			global::Gtk.Table.TableChild w4 = ((global::Gtk.Table.TableChild)(this.table1[this.password]));
+			w4.TopAttach = ((uint)(1));
+			w4.BottomAttach = ((uint)(2));
+			w4.LeftAttach = ((uint)(1));
+			w4.RightAttach = ((uint)(2));
+			w4.YOptions = ((global::Gtk.AttachOptions)(4));
+			// Container child table1.Gtk.Table+TableChild
+			this.username = new global::Gtk.Entry ();
+			this.username.CanFocus = true;
+			this.username.Name = "username";
+			this.username.IsEditable = true;
+			this.username.InvisibleChar = '●';
+			this.table1.Add (this.username);
+			global::Gtk.Table.TableChild w5 = ((global::Gtk.Table.TableChild)(this.table1[this.username]));
+			w5.LeftAttach = ((uint)(1));
+			w5.RightAttach = ((uint)(2));
+			w5.YOptions = ((global::Gtk.AttachOptions)(4));
+			w1.Add (this.table1);
+			global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(w1[this.table1]));
+			w6.Position = 0;
+			w6.Expand = false;
+			w6.Fill = false;
+			w6.Padding = ((uint)(5));
 			// Internal child Banshee.DoubanFM.Configuration.ActionArea
-			global::Gtk.HButtonBox w8 = this.ActionArea;
-			w8.Name = "ConfigurationDialog_ActionArea";
-			w8.Spacing = 10;
-			w8.BorderWidth = ((uint)(5));
-			w8.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(4));
+			global::Gtk.HButtonBox w7 = this.ActionArea;
+			w7.Name = "ConfigurationDialog_ActionArea";
+			w7.Spacing = 10;
+			w7.BorderWidth = ((uint)(5));
+			w7.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(4));
 			// Container child ConfigurationDialog_ActionArea.Gtk.ButtonBox+ButtonBoxChild
 			this.buttonCancel = new global::Gtk.Button ();
 			this.buttonCancel.CanDefault = true;
@@ -102,9 +99,9 @@ namespace Banshee.DoubanFM
 			this.buttonCancel.UseUnderline = true;
 			this.buttonCancel.Label = "gtk-cancel";
 			this.AddActionWidget (this.buttonCancel, -6);
-			global::Gtk.ButtonBox.ButtonBoxChild w9 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w8[this.buttonCancel]));
-			w9.Expand = false;
-			w9.Fill = false;
+			global::Gtk.ButtonBox.ButtonBoxChild w8 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w7[this.buttonCancel]));
+			w8.Expand = false;
+			w8.Fill = false;
 			// Container child ConfigurationDialog_ActionArea.Gtk.ButtonBox+ButtonBoxChild
 			this.buttonOk = new global::Gtk.Button ();
 			this.buttonOk.CanDefault = true;
@@ -114,15 +111,15 @@ namespace Banshee.DoubanFM
 			this.buttonOk.UseUnderline = true;
 			this.buttonOk.Label = "gtk-ok";
 			this.AddActionWidget (this.buttonOk, -5);
-			global::Gtk.ButtonBox.ButtonBoxChild w10 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w8[this.buttonOk]));
-			w10.Position = 1;
-			w10.Expand = false;
-			w10.Fill = false;
+			global::Gtk.ButtonBox.ButtonBoxChild w9 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w7[this.buttonOk]));
+			w9.Position = 1;
+			w9.Expand = false;
+			w9.Fill = false;
 			if ((this.Child != null)) {
 				this.Child.ShowAll ();
 			}
 			this.DefaultWidth = 309;
-			this.DefaultHeight = 130;
+			this.DefaultHeight = 163;
 			this.Show ();
 			this.buttonCancel.Pressed += new global::System.EventHandler (this.OnButtonCancelPressed);
 			this.buttonOk.Pressed += new global::System.EventHandler (this.OnButtonOkPressed);
